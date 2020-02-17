a64e42b56bc606a7b4158d59f14f84bda1e73392
Neil <neil.mf.benin@gmail.com>
2020-02-17 11:23:39 +0100

    starting to make classes

--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 
 //include all your model files here
-require 'Model/User.php';
+require 'Model/Customer.php';
+require 'Model/Product.php';
 //include all your controllers here
 require 'Controller/HomepageController.php';
 
