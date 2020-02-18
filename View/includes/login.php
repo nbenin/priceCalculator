a4b6f561a065669e83c626abac83f3f1f49de3ef
Neil <neil.mf.benin@gmail.com>
2020-02-18 12:20:56 +0100

    hmm

--- View/includes/login.php
+++ View/includes/login.php
@@ -1,17 +1,17 @@
 
 <div class="form-group">
     <label for="name">Name</label>
-    <input type="text" class="form-control" id="name" placeholder="Name">
+    <input type="text" class="form-control" id="name" name="name" placeholder="Name">
 </div>
 
 <div class="form-group">
     <label for="email">Email</label>
-    <input type="email" class="form-control" id="email" placeholder="Email">
+    <input type="email" class="form-control" id="email" name="email" placeholder="Email">
 </div>
 
 <div class="form-group">
     <label for="password">Password</label>
-    <input type="password" class="form-control" id="password" placeholder="Password">
+    <input type="password" class="form-control" id="password" name="password" placeholder="Password">
 </div>
 
 <button type="submit" class="btn btn-primary">Sign in</button>
