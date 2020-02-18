9254dbee98e49677db5c4ca2f04cc5aa189ae47f
Victor-28 <victorakintola@gmail.com>
2020-02-18 10:20:51 +0100

    loging form

---
+++ View/includes/login.php
@@ -0,0 +1,24 @@
+<form>
+
+    <div class="form-group">
+        <label for="inputEmail">Name</label>
+        <input type="email" class="form-control" id="inputName" placeholder="Name">
+    </div>
+
+    <div class="form-group">
+        <label for="inputEmail">Email</label>
+        <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+    </div>
+
+    <div class="form-group">
+        <label for="inputPassword">Password</label>
+        <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+    </div>
+
+    <div class="form-group">
+        <label class="form-check-label"><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-primary">Sign in</button>
+</form>
+
+
